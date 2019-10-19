799287fc050646cadb6f653c7fe07ef576bcfa9e
Anatoly <Toll1605@gmail.com>
2019-10-19 23:47:27 +0500

    Second homework done

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,44 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 30) == 225)
+            assert(Exercises.sumOfDivBy3Or5(5, 50) == 590)
+            assert(Exercises.sumOfDivBy3Or5(1, 10) == 33)
+        }
+
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(57) == Seq(3, 19))
+        }
+
+        'test_sortByHeavyweight - {
+            val balls: Map[String, (Int, Double)] =
+                Map(
+                    "Aluminum" -> (3, 2.5),
+                    "Gold" -> (4, 3.5),
+                    "Silver" -> (4, 3)
+            )
+            assert(Exercises.sortByHeavyweight(balls) == Seq("Aluminum", "Silver", "Gold"))
+        }
+
+        'test_sumScalars - {
+            val leftVec0 = Exercises.Vector2D(1, 0)
+            val leftVec1 = Exercises.Vector2D(0, 1)
+            val rightVec0 = Exercises.Vector2D(-1, 0)
+            val rightVec1 = Exercises.Vector2D(2, 0)
+            assert(Exercises.sumScalars(leftVec0, leftVec1, rightVec0, rightVec1) == -2)
+        }
+
+        'test_sumCosines - {
+            val leftVec0 = Exercises.Vector2D(1, 0)
+            val leftVec1 = Exercises.Vector2D(0, 1)
+            val rightVec0 = Exercises.Vector2D(-1, 0)
+            val rightVec1 = Exercises.Vector2D(2, 0)
+            assert(Exercises.sumCosines(leftVec0, leftVec1, rightVec0, rightVec1) == -1)
+        }
+
     }
 }
