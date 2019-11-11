4f637a9807697a8ee17600a53c645d3946fb7fc9
IvanovPetrVasilevch <Peter.1989@list.ru>
2019-11-12 00:18:43 +0500

    Иванов Петр Ф0-460001 ДЗ3

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,58 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        'test_prettyBooleanFormatter1 - {
+            assert(Exercises.prettyBooleanFormatter1(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter1(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter1(0) == "0")
+        }
+
+        'test_prettyBooleanFormatter2 - {
+            assert(Exercises.prettyBooleanFormatter2(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter2(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter2(0) == "0")
+        }
+
+        'test_prettyBooleanFormatter3 - {
+            assert(Exercises.prettyBooleanFormatter3(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter3(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter3(0) == "0")
+        }
+
+        'test_max1 - {
+            assert(Exercises.max1(Seq(20, 101, 94, 15)) == 101)
+            assert(Exercises.max1(Seq(-506, 0, 100081)) == 100081)
+            assert(Exercises.max1(Seq()) == 0)
+        }
+
+        'test_max2 - {
+            assert(Exercises.max2(Seq(20, 101, 94, 15)) == Seq(101))
+            assert(Exercises.max2(Seq(-506, 0, 100081)) == Seq(100081))
+            assert(Exercises.max2(Seq()) == Nil)
+        }
+
+        'test_max3 - {
+            assert(Exercises.max3(Seq(20, 101, 94, 15)) == Option(101))
+            assert(Exercises.max3(Seq(-506, 0, 100081)) == Option(100081))
+            assert(Exercises.max3(Seq()) == None)
+        }
+
+        'test_sum1 - {
+            assert(Exercises.sum1(2, 3) == 5)
+            assert(Exercises.sum1(10001, 10001) == 20002)
+            assert(Exercises.sum1(150, 500) == 650)
+        }
+
+        'test_sum2 - {
+            assert(Exercises.sum2(2, 3) == 5)
+            assert(Exercises.sum2(10001, 10001) == 20002)
+            assert(Exercises.sum2(150, 500) == 650)
+        }
+
+        'test_sum3 - {
+            assert(Exercises.sum3(2, 3) == 5)
+            assert(Exercises.sum3(10001, 10001) == 20002)
+            assert(Exercises.sum3(150, 500) == 650)
         }
     }
 }
