fbf0d6a5fea8fc4c366596a27c767ab9dd20ea11
iamrusskikh <leha-russ@rambler.ru>
2019-11-12 22:47:07 +0500

    Домашняя работа 3. Русских

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -9,5 +9,59 @@ object Test extends TestSuite{
             val trueStr = "правда"
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
         }
+
+        'test_prettyBooleanFormatter1 - {
+            assert(Exercises.prettyBooleanFormatter1(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter1(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter1(1) == "1")
+        }
+
+        'test_prettyBooleanFormatter2 - {
+            assert(Exercises.prettyBooleanFormatter2(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter2(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter2(1) == "1")
+        }
+
+        'test_prettyBooleanFormatter3 - {
+            assert(Exercises.prettyBooleanFormatter3(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter3(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter3(1) == "1")
+        }
+
+        'test_max1 - {
+            assert(Exercises.max1(Seq(10, 18)) == 18)
+            assert(Exercises.max1(Seq()) == -1)
+            assert(Exercises.max1(Seq(2, -1, 1)) == 2)
+        }
+
+        'test_max2 - {
+            assert(Exercises.max2(Seq(10, 18)) == Seq(18))
+            assert(Exercises.max2(Seq()) == Seq())
+            assert(Exercises.max2(Seq(2, -1, 1)) == Seq(2))
+        }
+
+        'test_max3 - {
+            assert(Exercises.max3(Seq(10, 18)).contains(18))
+            assert(Exercises.max3(Seq()).isEmpty)
+            assert(Exercises.max3(Seq(2, -1, 1)).contains(2))
+        }
+
+        'test_sum1 - {
+            assert(Exercises.sum1(1, 5) == 6)
+            assert(Exercises.sum1(2, 12) == 14)
+            assert(Exercises.sum1(3, 95) == 98)
+        }
+
+        'test_sum2 - {
+            assert(Exercises.sum2(1, 5) == 6)
+            assert(Exercises.sum2(2, 12) == 14)
+            assert(Exercises.sum2(3, 95) == 98)
+        }
+
+        'test_sum3 - {
+            assert(Exercises.sum3(1, 5) == 6)
+            assert(Exercises.sum3(2, 12) == 14)
+            assert(Exercises.sum3(3, 95) == 98)
+        }
     }
 }
