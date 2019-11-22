f173ca7441166a6df21c1950ac5cfcd19183caaf
Underwindow <35759068+Underwindow@users.noreply.github.com>
2019-11-22 19:01:37 +0500

    Add files via upload

---
+++ homeworks/homework_4/src/main/scala/Table.scala
@@ -0,0 +1,14 @@
+case class Table(width: Int, height: Int) {
+  private val cells: scala.collection.mutable.Seq[Cell] =
+    scala.collection.mutable.Seq.fill(width * height)(new EmptyCell)
+
+  def getCell(ix: Int, iy: Int): Option[Cell] = {
+    if (ix < 0 || ix >= width || iy < 0 || iy >= height) None
+    else Some(cells(ix + iy * width))
+  }
+
+  def setCell(ix: Int, iy: Int, cell: Cell): Unit = {
+    if (ix >= 0 && ix < width && iy >= 0 && iy < height)
+      cells(ix + iy * width) = cell
+  }
+}
