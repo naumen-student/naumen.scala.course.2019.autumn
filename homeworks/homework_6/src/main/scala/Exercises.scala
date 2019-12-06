819a2d9408c5dbc0f0d86f65ef297fe8858d1446
JulianaOsi <33633916+JulianaOsi@users.noreply.github.com>
2019-12-06 16:15:20 +0500

    Update Exercises.scala

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,11 @@
 object Exercises {
-
-
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    var res: Seq[T] = Seq()
+    for (el <- seq)
+      res = el +: res
+    res
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,19 +13,44 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    idx match {
+      case _ if idx <= 1 => return idx
+      case _ => fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
+    }
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = for (i <- 0 until idx + 1) yield fibonacci4Index(i)
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = {
+    text.toUpperCase.toCharArray.map(
+      char => if (MORSE.contains(char.toString))
+        MORSE(char.toString)
+      else
+        char.toString).mkString(" ")
+  }
 
-
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = {
+    var res = text
+    var reversedWord = new String()
+    for(word <- text.split("[.,!?;: ]+")) {
+      reversedWord = word
+      if (word.head.isUpper) {
+        reversedWord = reversedWord.reverse
+        reversedWord = reversedWord.substring(0, 1).toUpperCase +
+          reversedWord.substring(1, word.length - 1) +
+          reversedWord.substring(word.length - 1, word.length).toLowerCase
+      }
+      else
+        reversedWord = reversedWord.reverse
+      res = res.replace(word, reversedWord)
+    }
+    res
+  }
 }
