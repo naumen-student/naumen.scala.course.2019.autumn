5af6d3c92d1d920df001ca03f04ffea555b8b7db
Ilya <hoconbipka174rus@gmail.com>
2019-12-06 19:01:05 +0500

    homework 6 done

---
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -0,0 +1,35 @@
+import utest._
+
+
+object Test extends TestSuite{
+
+  val tests = Tests{
+    'reverse - {
+      assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
+      assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
+    }
+
+    'fibonacci4Index - {
+      assert(Exercises.fibonacci4Index(2) == 1)
+      assert(Exercises.fibonacci4Index(5) == 5)
+      assert(Exercises.fibonacci4Index(-2) == -1)
+    }
+
+    'fibonacci - {
+      assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
+      assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
+      assert(Exercises.fibonacci(-2) == Seq(-1, 1, 0))
+    }
+
+    'morse - {
+      assert(Exercises.morse("SOS") == "... --- ...")
+      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
+    }
+
+    'wordReverse - {
+      assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
+    }
+
+
+  }
+}
