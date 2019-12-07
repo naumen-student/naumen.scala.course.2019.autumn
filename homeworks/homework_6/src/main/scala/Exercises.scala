9dc5d6dc8cd06442da5440df4e3a00b991331f94
iamrusskikh <leha-russ@rambler.ru>
2019-12-07 20:46:22 +0500

    Homework6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,11 @@
+import scala.collection.mutable
+
 object Exercises {
 
-
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq match {
+    case Nil => seq
+    case _ => seq.last :: reverse(seq.init).toList
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +13,16 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = fibonacci(idx).last
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = idx match {
+    case _ if idx < 2 => Seq(0, 1).take(idx + 1)
+    case _ => {
+      val range = mutable.MutableList.range(1, idx)
+      for (i <- range.drop(2)) range(i - 1) = range(i - 3) + range(i - 2)
+      0 :: 1 :: range.toList
+    }
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +30,32 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = {
+    text
+      .toUpperCase
+      .map(ch => morseEncode(ch.toString))
+      .mkString(" ")
+  }
+
+  private def morseEncode(ch: String): String = {
+    if (MORSE.contains(ch.toString))
+      MORSE(ch.toString)
+    else ch.toString
+  }
 
 
-  def wordReverse(text: String): String = ???
+
+
+  def wordReverse(string: String): String = {
+    def revCap(s: String): String =
+      s.headOption match {
+        case Some(c) if c.isUpper =>
+          (c.toLower +: s.drop(1)).reverse.capitalize
+        case Some(c) if c.isLower =>
+          s.reverse
+        case _ => s
+      }
+    string.split("\\b").map(revCap).mkString("")
+  }
 
 }
